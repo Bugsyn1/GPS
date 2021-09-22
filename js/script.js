f3ec7729923fd37ccf545bdc28a255e13725e25e
Pavel <lemolight@gmail.com>
2021-09-22 11:28:03 +0300

    mod

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ let init = false;
 let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
 let tablet = window.matchMedia('(min-width: 768px) and (max-width: 1119px)');
 let desktop = window.matchMedia('(min-width: 1120px)');
+
 function swiperMode() {
-   
-    if(mobile.matches) {
+
+    if (mobile.matches) {
         if (!init) {
             init = true;
             swiper = new Swiper(".mySwiper", {
@@ -15,47 +16,57 @@ function swiperMode() {
                     el: ".swiper-pagination",
                     clickable: true,
                 },
-        
+
                 autoplay: {
                     delay: 1500,
                     disableOnInteration: true,
                     pauseOnMouseEnter: true,
                 },
-        
-          });
+
+            });
         }
-    }
-    else if(tablet.matches) {
+    } else if (tablet.matches) {
         swiper.destroy();
         init = false;
-    }
-    else if(desktop.matches) {
+    } else if (desktop.matches) {
         swiper.destroy();
         init = false;
     }
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     swiperMode();
 });
 
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     swiperMode();
-}); 
+});
 
+
+let allElem = document.querySelectorAll('.swiper-slide');
+if (tablet.matches) {
+    for (let i = allElem.length - 1; i >= 6; i--) {
+          
+    allElem[i].classList.add('hide');
+
+    }
+
+
+}
 
 let btn = document.querySelector('.show-all');
-let swiperWrapper = document.querySelector('.swiper-wrapper')
+let swiperSlide = document.querySelectorAll('.hide')
 let open = function () {
-    swiperWrapper.classList.toggle('swiper-open');
-    swiperWrapper.classList.toggle('wrapper-hide');
-    if(btn.innerHTML == 'Скрыть'){
-        btn.innerHTML = 'Показать все';
+    for (let i = 0; i < swiperSlide.length; i++) {
+        if (swiperSlide[i].classList.contains('hide')) {
+            btn.textContent = 'Скрыть';
+        } else {
+            btn.textContent = 'Показать все';
+        }
+        swiperSlide[i].classList.toggle('hide');
+        btn.classList.toggle('btn-hide');
     }
-    else {
-btn.innerHTML = 'Скрыть'
 
-    }
 }
 btn.addEventListener('click', open)
